f46a3a2ee61ee7614e7ab1a8d3d4e3d7fe5d9979
Ragath <Ragath@outlook.com>
2023-12-03 16:36:46 +0100

    Add project files.

---
+++ Bresenham.Tests/BresenhamLineTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Bresenham;
+
+[TestClass]
+public class BresenhamLineTests
+{
+    [TestMethod]
+    [DataRow(0, 0, 0, 0)]
+    [DataRow(-3, 2, 3, -2)]
+    public void TraverseTest(int x, int y, int x2, int y2)
+    {
+        var resultA = new List<(int x, int y)>();
+        var resultC = new (int x, int y)[100].AsSpan();
+        BresenhamLine.Traverse(x, y, x2, y2, (x, y) => resultA.Add((x, y)));
+        var resultB = BresenhamLine.Traverse(x, y, x2, y2).ToList();
+        
+            BresenhamLine.Traverse(x, y, x2, y2, ref resultC);
+
+        Assert.IsNotNull(resultA);
+        Assert.IsTrue(resultA.Any());
+        CollectionAssert.AreEqual(resultA, resultB);
+        CollectionAssert.AreEqual(resultA, resultC[..resultA.Count].ToArray());
+    }
+}
---
+++ Bresenham/BresenhamLine.cs
@@ -0,0 +1,127 @@
+﻿namespace Bresenham;
+
+public static class BresenhamLine
+{
+    public static void Traverse(int x, int y, int x2, int y2, Action<int, int> action)
+    {
+        var w = x2 - x;
+        var h = y2 - y;
+
+        var dx1 = Math.Sign(w);
+        var dx2 = dx1;
+
+        var dy1 = Math.Sign(h);
+        var dy2 = 0;
+
+        var longest = Math.Abs(w);
+        var shortest = Math.Abs(h);
+        if (longest <= shortest)
+        {
+            longest = Math.Abs(h);
+            shortest = Math.Abs(w);
+
+            dy2 = dy1;
+            dx2 = 0;
+        }
+
+        var numerator = longest >> 1;
+        for (var i = 0; i <= longest; i++)
+        {
+            action(x, y);
+            numerator += shortest;
+            if (numerator < longest)
+            {
+                x += dx2;
+                y += dy2;
+            }
+            else
+            {
+                numerator -= longest;
+                x += dx1;
+                y += dy1;
+            }
+        }
+    }
+
+    public static void Traverse(int x, int y, int x2, int y2, ref Span<(int x, int y)> result)
+    {
+        var w = x2 - x;
+        var h = y2 - y;
+
+        var dx1 = Math.Sign(w);
+        var dx2 = dx1;
+
+        var dy1 = Math.Sign(h);
+        var dy2 = 0;
+
+        var longest = Math.Abs(w);
+        var shortest = Math.Abs(h);
+        if (longest <= shortest)
+        {
+            longest = Math.Abs(h);
+            shortest = Math.Abs(w);
+
+            dy2 = dy1;
+            dx2 = 0;
+        }
+
+        var numerator = longest >> 1;
+        for (var i = 0; i <= longest; i++)
+        {
+            result[i] = (x, y);
+            numerator += shortest;
+            if (numerator < longest)
+            {
+                x += dx2;
+                y += dy2;
+            }
+            else
+            {
+                numerator -= longest;
+                x += dx1;
+                y += dy1;
+            }
+        }
+    }
+
+    public static IEnumerable<(int x, int y)> Traverse(int x, int y, int x2, int y2)
+    {
+        var w = x2 - x;
+        var h = y2 - y;
+
+        var dx1 = Math.Sign(w);
+        var dx2 = dx1;
+
+        var dy1 = Math.Sign(h);
+        var dy2 = 0;
+
+        var longest = Math.Abs(w);
+        var shortest = Math.Abs(h);
+        if (longest <= shortest)
+        {
+            longest = Math.Abs(h);
+            shortest = Math.Abs(w);
+
+            dy2 = dy1;
+            dx2 = 0;
+        }
+
+        var numerator = longest >> 1;
+        for (var i = 0; i <= longest; i++)
+        {
+            yield return (x, y);
+            numerator += shortest;
+            if (numerator < longest)
+            {
+                x += dx2;
+                y += dy2;
+            }
+            else
+            {
+                numerator -= longest;
+                x += dx1;
+                y += dy1;
+            }
+        }
+    }
+}
